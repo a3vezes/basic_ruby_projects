5e62e3007801ab44b09f5078857f529578c8a6dd
a3vezes <tucaariza@gmail.com>
2022-01-26 20:03:13 -0300

    Substrings

--- 2.Sub Strings/main.rb
+++ 2.Sub Strings/main.rb
@@ -1,14 +1,10 @@
-def substrings(strings, dictionary)
-  dictionary.reduce(Hash.new(0)) do |result, word|
-    result[word] += 1 if strings.downcase.include? word
-    result
+class Substrings
+  def substrings(strings, dictionary)
+    dictionary.reduce(Hash.new(0)) do |result, word|
+      strings.split(' ').each do |s|
+        result[word] += 1 if s.downcase.include? word
+      end  
+      result
+    end
   end
 end
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-#puts substrings("below", dictionary)
-# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
-
-# h1 = { a: 100, b: 200 }
-# h2 = { a: 246, b: 300 }
-# puts h1.merge(h2)
---
+++ 2.Sub Strings/spec/substrings.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# spec/substrings.rb
+require '../main.rb'
+
+describe Substrings do
+  subject { Substrings.new }
+
+  describe '#substrings' do
+    it 'works with a empty dictionary' do
+      expect(subject.substrings('A B C', [])).to eql({})
+    end
+
+    it 'works with a single character' do
+      expect(subject.substrings('a', ['a'])).to eql({'a' => 1})
+    end
+
+    it 'works with multiple single characters' do
+      expect(subject.substrings('a b c', ['a', 'b', 'c'])).to eql({'a' => 1, 'b' => 1, 'c' => 1})
+    end
+
+    it 'works with multiple cases' do
+      expect(subject.substrings('A b C', ['a', 'b', 'c'])).to eql({'a' => 1, 'b' => 1, 'c' => 1})
+    end
+
+    it 'works with a string with punctuation' do
+      expect(subject.substrings('a! b? c:', ['a', 'b', 'c'])).to eql({'a' => 1, 'b' => 1, 'c' => 1})
+    end
+
+    it 'works with a single word' do
+      expect(subject.substrings('below', ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])).to eql({"below" => 1, "low" => 1})
+    end
+
+    it 'works with multiple words' do
+      expect(subject.substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])).to eql({"down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1})
+    end
+  end
+end
