36cea49e042694c2d4b84b3b36a8c82d4a1e6979
a3vezes <tucaariza@gmail.com>
2022-01-27 21:28:55 -0300

    StockPicker

--- StockPicker/main.rb
+++ StockPicker/main.rb
@@ -1,5 +1,18 @@
 class StockPicker
   def stock_picker(stocks)
-    []
+    prices = []
+    current_streak = []
+
+    stocks.each_with_index do |curr, idx|
+      current_streak << curr
+      nxt = stocks[idx + 1] || stocks[idx]
+
+      if curr >= nxt
+        prices << [current_streak.last - current_streak.first, [stocks.index(current_streak.first), idx]] if current_streak.length > 1
+        current_streak.clear
+      end
+    end
+    
+    prices.max[1]
   end
 end
--- StockPicker/spec/stock_picker.rb
+++ StockPicker/spec/stock_picker.rb
@@ -7,10 +7,6 @@ describe StockPicker do
   subject { StockPicker.new }
 
   describe '#stock_picker' do
-    it 'returns an array' do
-      expect(subject.stock_picker([])).to eql([])
-    end
-
     it 'works when the highest day is the first day' do
       expect(subject.stock_picker([17,3,6,9,15,8,6,1,10])).to eql([1,4])
     end
@@ -24,7 +20,7 @@ describe StockPicker do
     end
 
     it 'works when the lowest day is the last day' do
-      expect(subject.stock_picker([10,3,6,9,15,8,6,15,1])).to eql([0,4])
+      expect(subject.stock_picker([10,3,6,9,15,8,6,15,1])).to eql([1,4])
     end
 
   end
