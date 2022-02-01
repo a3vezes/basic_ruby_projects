919e1c8580234c9c0f91750999b7a65082d07015
a3vezes <tucaariza@gmail.com>
2022-02-01 19:26:16 -0300

    StringCompressor - Compress

--- StringCompressor/string_compressor.rb
+++ StringCompressor/string_compressor.rb
@@ -1,3 +1,7 @@
 class StringCompressor
-  def compress; end
+  def compress(string)
+    result = ''
+    string.scan(/(.)\1{1,8}/) { result << Regexp.last_match(0).length.to_s + Regexp.last_match(0)[0] }
+    result
+  end
 end
