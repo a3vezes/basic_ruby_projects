ace0e583f7474c702ead9ddad4cbc3fcae7d6616
a3vezes <tucaariza@gmail.com>
2022-02-03 20:46:43 -0300

    Hangman - ClearWords

---
+++ Hangman/utils/clear_words.rb
@@ -0,0 +1,25 @@
+class ClearWords
+  def self.open_input(input)
+    File.open(input, 'r')
+  end
+
+  def self.get_clear_words(words)
+    words.select { |word| word.length.between?(5, 12) }
+  end
+
+  def self.create_output(words, output)
+    File.open(output, 'w') do |file|
+      file.puts words
+    end
+  end
+
+  def self.clear_words(input, output)
+    input_file = open_input(input)
+
+    clear_words = get_clear_words(input_file)
+
+    create_output(clear_words, output)
+  end
+end
+
+ClearWords.clear_words('all_words.txt', 'clear_words.txt')
