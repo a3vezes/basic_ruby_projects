f93232d9eb55c702a5d636fa4a390fbc5202c8c6
a3vezes <tucaariza@gmail.com>
2022-02-03 21:08:06 -0300

    Hangman - Display module

---
+++ Hangman/lib/display.rb
@@ -0,0 +1,17 @@
+module Display
+  def menu_options
+    puts 'Welcome to Hangman'
+    puts 'Press (1) to play'
+    puts 'Press (2) to your score'
+    puts 'Press (3) to load a saved game'
+    puts 'Press (4) to quit'
+  end
+
+  def clear_console
+    puts `clear`
+  end
+
+  def invalid_option
+    puts 'Please input a valid option'
+  end
+end
