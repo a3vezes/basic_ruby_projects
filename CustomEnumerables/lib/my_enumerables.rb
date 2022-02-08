618dba731113ca78e27b4035c78497f392f81627
a3vezes <tucaariza@gmail.com>
2022-02-07 21:52:52 -0300

    Custom Enumerables

--- CustomEnumerables/lib/my_enumerables.rb
+++ CustomEnumerables/lib/my_enumerables.rb
@@ -8,4 +8,58 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    size.times { |idx| yield self[idx] }
+    self
+  end
+
+  def my_each_with_index
+    size.times { |idx| yield(self[idx], idx) }
+    self
+  end
+
+  def my_select
+    result = []
+    size.times { |idx| result << self[idx] if yield self[idx] }
+    result
+  end
+
+  def my_all?
+    size.times { |idx| return false unless yield self[idx] }
+    true
+  end
+
+  def my_any?
+    size.times { |idx| return true if yield self[idx] }
+    false
+  end
+
+  def my_none?
+    size.times { |idx| return false if yield self[idx] }
+    true
+  end
+
+  def my_count
+    return size unless block_given?
+
+    count = 0
+    size.times { |idx| count += 1 if yield self[idx] }
+    count
+  end
+
+  def my_map
+    result = []
+
+    size.times { |idx| result.push(yield self[idx]) }
+
+    result
+  end
+
+  def my_inject(initial_value = 0)
+    sum = initial_value
+
+    size.times { |idx| sum = yield(sum, self[idx]) }
+
+    sum
+  end
 end
--- readme.md
+++ readme.md
@@ -6,10 +6,12 @@
 
 ## Sumary
 
-### _Algorithms_ : Caesar Cipher, Sub Strings, Stock Picker, Bubble Sort, String Compressor
+### _Algorithms_ : Caesar Cipher, Sub Strings, Stock Picker, Bubble Sort, String Compressor, Custom Enumerables
 
 ### _OOP_ : Tic Tac Toe, Event Manager, Hangman
 
 ### _Rspec/TDD_ : Caesar Cipher, Stock Picker, String Compressor, Sub Strings
 
 ### _Files And Serialization_ : Event Manager, Hangman
+
+### _Blocks/Yield_ : Custom Enumerables
