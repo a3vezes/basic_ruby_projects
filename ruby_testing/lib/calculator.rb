e97b9ca27868b6d6e4f8763b94f12a5422b5fcb6
a3vezes <tucaariza@gmail.com>
2022-02-13 02:01:52 -0300

    Calculator TDD

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -2,4 +2,18 @@ class Calculator
   def add(*numbers)
     numbers.sum
   end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def divide(a, b)
+    a / b
+  rescue ZeroDivisionError
+    'I can\'t divide by zero'
+  end
 end
--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -11,10 +11,47 @@ describe Calculator do
       calculator = Calculator.new
       expect(calculator.add(5, 2)).to eql(7)
     end
+
+    it 'returns the sum of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
   end
 
-  it 'returns the sum of more than two numbers' do
-    calculator = Calculator.new
-    expect(calculator.add(2, 5, 7)).to eql(14)
+  describe '#multiply' do
+    it 'returns the multiplication of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(3, 7)).to eql(21)
+    end
+  end
+
+  describe '#subtract' do
+    it 'returns the subtraction of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(8, 4)).to eql(4)
+    end
+
+    it 'returns a negative number, if the first number is smaller' do
+      calculator = Calculator.new
+      expect(calculator.subtract(4, 8)).to eql(-4)
+    end
+  end
+
+  describe '#divide' do
+    it 'returns the division of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(6, 3)).to eql(2)
+    end
+
+    it 'doesn\'t raises an error when dividing by 0' do
+      calculator = Calculator.new
+      # { } creates a block to monitor for the exception to be raised
+      expect { calculator.divide(5, 0) }.to_not raise_error
+    end
+
+    it 'returns a message when dividing by 0' do
+      calculator = Calculator.new
+      expect(calculator.divide(5, 0)).to eql('I can\'t divide by zero')
+    end
   end
 end
