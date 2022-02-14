afbb55bdddf2aec362299c00334a48fbc243b4a1
a3vezes <tucaariza@gmail.com>
2022-02-13 22:37:36 -0300

    10 to 13 Done

--- RubyTesting/spec/11b_cat_spec.rb
+++ RubyTesting/spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:apollo) { described_class.new("Apollo", 'Siamese', 'Dark Brown')}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when using basic shared examples ' do
+    include_examples "shared method name"
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(apollo).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(apollo).to be_hiding
   end
 end
